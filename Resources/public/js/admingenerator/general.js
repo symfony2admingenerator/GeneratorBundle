ea5c569d48d85d437fb5eb1446acb8b406083e80
loostro <loostro@gmail.com>
2014-08-17 19:58:27 +0200

    Add loading modal

--- Resources/public/js/admingenerator/general.js
+++ Resources/public/js/admingenerator/general.js
@@ -6,6 +6,12 @@ $(document).ready(function(){
         container: 'body'
     });
 
+    $('#admingenerator_loading_modal').modal({
+    	backdrop: 'static',
+    	keyboard: false,
+    	show: false
+    });
+
     // Moved all scripts to dedicated widgets
     // Do not remove, this file is used when developing new features
 });
