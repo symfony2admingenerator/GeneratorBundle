9affa0f6181377eee7a299728bf989ad7cd82f1e
loostro <loostro@gmail.com>
2014-09-12 05:59:15 +0200

    Hotfix
    Fix argument order in DoctrineODMQueryFilter

--- QueryFilter/DoctrineODMQueryFilter.php
+++ QueryFilter/DoctrineODMQueryFilter.php
@@ -16,7 +16,7 @@ class DoctrineODMQueryFilter extends BaseQueryFilter
             list($field, $value) = $args;
             $param = $this->getUniqueName();
             $this->query->andWhere($this->getComparison($field, $matches['operator'], $param));
-            $this->query->setParameter($param, $this->formatValue($value, $matches['operator'], $field));
+            $this->query->setParameter($param, $this->formatValue($field, $matches['operator'], $value));
         }
     }
 
