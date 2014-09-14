bf974d7a4d924c940f36935a0066ac6d860425d0
loostro <loostro@gmail.com>
2014-09-14 13:23:21 +0200

    Move script to seperate file

--- Resources/public/js/admingenerator/general.js
+++ Resources/public/js/admingenerator/general.js
@@ -5,15 +5,6 @@ $(document).ready(function(){
     $('a[rel=tooltip]').tooltip({
         container: 'body'
     });
-    
-    // enable iScroll for list table
-    var listTable = new IScroll('.list-table-wrapper', {
-        scrollX: true,
-        scrollY: false,
-        mouseWheel: true,
-        scrollbars: true
-    });
-    $(document).on('touchmove', function(e) { e.preventDefault(); }, false);
 
     // Moved all scripts to dedicated widgets
     // Do not remove, this file is used when developing new features
