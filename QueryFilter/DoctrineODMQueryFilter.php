0e0fac68e0459046821fbf10fbd57ed815175840
loostro <loostro@gmail.com>
2014-09-28 20:53:09 +0200

    Do not require value for null comparison

--- QueryFilter/DoctrineODMQueryFilter.php
+++ QueryFilter/DoctrineODMQueryFilter.php
@@ -20,12 +20,12 @@ class DoctrineODMQueryFilter extends BaseQueryFilter
         }
     }
 
-    public function addIsNullFilter($field, $value)
+    public function addIsNullFilter($field, $value = null)
     {
         $this->query->andWhere($this->getComparison($field, 'IsNull'));
     }
 
-    public function addIsNotNullFilter($field, $value)
+    public function addIsNotNullFilter($field, $value = null)
     {
         $this->query->andWhere($this->getComparison($field, 'IsNotNull'));
     }
