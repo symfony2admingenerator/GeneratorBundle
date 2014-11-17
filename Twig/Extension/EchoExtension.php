93b8568ca9491db043d1611881536a6dedfdc274
Stéphane Escandell <stephane.escandell@gmail.com>
2014-11-18 00:17:26 +0100

    Moving Twig extensions to the new Symfony2admingenerator Twg Generator library

--- Twig/Extension/EchoExtension.php
+++ Twig/Extension/EchoExtension.php
@@ -5,64 +5,29 @@ namespace Admingenerator\GeneratorBundle\Twig\Extension;
 /**
  * @author Cedric LOMBARDOT
  * @author Piotr Gołębiewski <loostro@gmail.com>
+ * @author Stéphane Escandell
  */
 class EchoExtension extends \Twig_Extension
 {
-    protected $loader;
-    protected $controller;
-    protected $blockNames = array();
-
-    public function __construct(\Twig_LoaderInterface $loader)
-    {
-        $this->loader = $loader;
-    }
-
-    public function setController($controller)
-    {
-        $this->controller = $controller;
-    }
-
     /**
      * {@inheritdoc}
      */
     public function getFunctions()
     {
         return array(
-            'echo_twig'           => new \Twig_Function_Method($this, 'getEchoTwig'),
-            'echo_block'          => new \Twig_Function_Method($this, 'getEchoBlock'),
-            'echo_endblock'       => new \Twig_Function_Method($this, 'getEchoEndBlock'),
-            'echo_for'            => new \Twig_Function_Method($this, 'getEchoFor'),
-            'echo_endfor'         => new \Twig_Function_Method($this, 'getEchoEndFor'),
-            'echo_raw'            => new \Twig_Function_Method($this, 'getEchoRaw'),
-            'echo_endraw'         => new \Twig_Function_Method($this, 'getEchoEndRaw'),
-            'echo_spaceless'      => new \Twig_Function_Method($this, 'getEchoSpaceless'),
-            'echo_endspaceless'   => new \Twig_Function_Method($this, 'getEchoEndSpaceless'),
-            'echo_extends'        => new \Twig_Function_Method($this, 'getEchoExtends'),
-            'echo_if'             => new \Twig_Function_Method($this, 'getEchoIf'),
             'echo_if_granted'     => new \Twig_Function_Method($this, 'getEchoIfGranted'),
-            'echo_else'           => new \Twig_Function_Method($this, 'getEchoElse'),
-            'echo_elseif'         => new \Twig_Function_Method($this, 'getEchoElseIf'),
-            'echo_endif'          => new \Twig_Function_Method($this, 'getEchoEndIf'),
             'echo_path'           => new \Twig_Function_Method($this, 'getEchoPath'),
-            'echo_set'            => new \Twig_Function_Method($this, 'getEchoSet'),
             'echo_trans'          => new \Twig_Function_Method($this, 'getEchoTrans'),
-            'echo_twig_arr'       => new \Twig_Function_Method($this, 'getEchoTwigArr'),
-            'echo_twig_assoc'     => new \Twig_Function_Method($this, 'getEchoTwigAssoc'),
-            'echo_twig_filter'    => new \Twig_Function_Method($this, 'getEchoTwigFilter'),
-            'echo_include'        => new \Twig_Function_Method($this, 'getEchoInclude'),
-            'echo_use'            => new \Twig_Function_Method($this, 'getEchoUse'),
-            'echo_print_block'    => new \Twig_Function_Method($this, 'getEchoPrintBlock'),
-            'echo_render'         => new \Twig_Function_Method($this, 'getEchoRender'),
-            'char'                => new \Twig_Function_Method($this, 'char'),
+            'echo_render'         => new \Twig_Function_Method($this, 'getEchoRender')
         );
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getFilters()
     {
         return array(
-            'as_php'          => new \Twig_Filter_Method($this, 'asPhp'),
-            'php_name'        => new \Twig_Filter_Method($this, 'phpName'),
-            'wrap'            => new \Twig_Filter_Method($this, 'wrap'),
             'convert_as_form' => new \Twig_Filter_Method($this, 'convertAsForm'),
             'mapBy'           => new \Twig_Filter_Method($this, 'mapBy'),
             'flatten'         => new \Twig_Filter_Method($this, 'flatten'),
@@ -70,15 +35,118 @@ class EchoExtension extends \Twig_Extension
     }
 
     /**
-    * Try to convert options of form given as string from yaml to a good object
-    *
-    * eg type option for collection type
-    *
-    * @param string $options the string as php
-    * @param string $formType the form type
-    *
-    * @return string the new options
-    */
+     * Print "trans" tag for string $str with parameters $parameters
+     * for catalog $catalog.
+     *
+     * @param $str
+     * @param array $parameters
+     * @param string $catalog
+     * @return string
+     */
+    public function getEchoTrans($str, array $parameters = array(), $catalog = 'Admingenerator')
+    {
+        $echo_parameters=null;
+        $bag_parameters=array();
+
+        if ($parameterBag = $this->getParameterBag($str)) {
+            $str = $parameterBag['string'];
+            $bag_parameters = $parameterBag['params'];
+        }
+
+        if (!empty($parameters) || !empty($bag_parameters)) {
+            $echo_parameters="with {";
+
+            foreach ($parameters as $key => $value) {
+                $echo_parameters.= "'%".$key."%': '".$value."',";
+            }
+            foreach ($bag_parameters as $key => $value) {
+                $echo_parameters.= "'%".$key."%': ".$value.",";
+            }
+
+            $echo_parameters.="} ";
+        }
+
+        return '{% trans '.$echo_parameters.'from "'.$catalog.'" %}'.$str.'{% endtrans %}';
+    }
+
+    /**
+     * Print "echo tag with path call" to the path $path with params $params.
+     *
+     * @param $path
+     * @param array $params
+     * @param array|string $filters
+     * @return string
+     */
+    public function getEchoPath($path, $params = null, $filters = null)
+    {
+        if (null === $params) {
+            return (null === $filters)
+                ? strtr('{{ path("%%path%%") }}', array('%%path%%' => $path))
+                : strtr(
+                    '{{ path("%%path%%")|%%filters%% }}',
+                    array(
+                        '%%path%%' => $path,
+                        '%%filters%%' => (is_array($filters) ? implode('|', $filters) : $filters)
+                    )
+                );
+        }
+
+        $params = preg_replace('/\{\{\s+?([\w\.]+)\s+?\}\}/i', '$1', $params);
+
+        return (null === $filters)
+            ? strtr('{{ path("%%path%%", %%params%%) }}', array('%%path%%' => $path, '%%params%%' => $params))
+            : strtr(
+                '{{ path("%%path%%", %%params%%)|%%filters%% }}',
+                array(
+                    '%%path%%' => $path,
+                    '%%params%%' => $params,
+                    '%%filters%%' => (is_array($filters) ? implode('|', $filters) : $filters)
+                )
+            );
+    }
+
+    /**
+     * Print "if" tag with condition to is_expr_granted('$credentials')
+     * If $modelName is not null, append the $modelName to the function call.
+     *
+     * @param $credentials
+     * @param null $modelName
+     * @return string
+     */
+    public function getEchoIfGranted($credentials, $modelName = null)
+    {
+        return sprintf(
+            "{%% if is_expr_granted('%s'%s) %%}",
+            $credentials,
+            $modelName ? ', ' . $modelName : ''
+        );
+    }
+
+    /**
+     * Print "echo tag with render call" to the controller $controller
+     * with $params parameters.
+     *
+     * @param $controller
+     * @param array $params
+     * @return string
+     */
+    public function getEchoRender($controller, array $params = array())
+    {
+        $params = $this->getTwigAssociativeArray($params);
+
+        return '{{ render(controller("'.$controller.'", '.$params.')) }}';
+    }
+
+    /**
+     * Try to convert options of form given as string from yaml to a good object
+     *
+     * eg type option for collection type
+     *
+     * @param string $options the string as php
+     * @param string $formType the form type
+     *
+     * @return string the new options
+     */
     public function convertAsForm($options, $formType)
     {
         // Transforms PHP call into PHP (simple copy/paste)
@@ -135,66 +203,10 @@ class EchoExtension extends \Twig_Extension
         return $options;
     }
 
-    public function asPhp($variable)
-    {
-        if (!is_array($variable)) {
-            return $this->export($variable);
-        }
-
-        $str = $this->export($variable);
-
-        preg_match_all('/[^> ]+::__set_state\(array\((.+),\'loaded/i', $str, $matches);
-
-        if (isset($matches[1][0])) {
-            $params = 'return array('.$matches[1][0].')';
-            $params = eval($params. '?>');
-
-            $str_param = '';
-            foreach ($params as $p) {
-                if ('' !== $str_param) {
-                    $str_param .= ', ';
-                }
-                $str_param .= $this->export($p);
-            }
-
-            $str = preg_replace("/([^> ]+)::__set_state\(/i", ' new \\\$0', $str);
-            $str = str_replace('::__set_state', '', $str);
-            $str = str_replace('array('.$matches[1][0].',\'loaded\' => false,  )', $str_param, $str);
-        }
-
-        return $str;
-
-    }
-
-    /**
-     * Converts string into valid PHP function name
-     *
-     * @param string $str
-     * @return string
-     */
-    public function phpName($str)
-    {
-        $str = preg_replace('/[^\w]+/', '', $str);
-
-        return $str;
-    }
-
-    /**
-     * Wrap string around with given string only if string is not empty
-     *
-     * @param string $str
-     * @param string $wrap
-     * @return string
-     */
-    public function wrap($str, $wrap)
-    {
-        return (is_string($str) && $str !== '') ? $wrap.$str.$wrap : $str;
-    }
-
     /**
      * Map collection by key. For objects, return the property, use
      * get method or is method, if avaliable.
-     * 
+     *
      * @param  array  $input Array of arrays or objects.
      * @param  string $key   Key to map by.
      * @throws \InvalidArgumentException If array item is not an array or object.
@@ -236,7 +248,7 @@ class EchoExtension extends \Twig_Extension
 
     /**
      * Flatten nested arrays.
-     * 
+     *
      * @param  array $input Array of arrays.
      * @return array Flat array.
      */
@@ -246,34 +258,6 @@ class EchoExtension extends \Twig_Extension
             new \RecursiveArrayIterator($input)
         );
         return iterator_to_array($it, false);
-    }
-
-    
-    public function export($variable)
-    {
-        return str_replace(array("\n", 'array (', '     '), array('', 'array(', ''), var_export($variable, true));
-    }
-
-    /**
-     * Get characters by code
-     *
-     * Note: if the code is higher than 256, it will return the code mod 256.
-     * For example: chr(321)=A because A=65(256)
-     *
-     * @param integer Any number of integer codes
-     * @return string
-     */
-    public function char()
-    {
-        $str = '';
-
-        foreach (func_get_args() as $char) {
-            if (is_int($char)) {
-                $str .= chr($char);
-            }
-        }
-
-        return $str;
     }
 
     /**
@@ -403,206 +387,6 @@ class EchoExtension extends \Twig_Extension
         return false;
     }
 
-    public function getEchoTrans($str, array $parameters = array(), $catalog = 'Admingenerator')
-    {
-        $echo_parameters=null;
-        $bag_parameters=array();
-
-        if ($parameterBag = $this->getParameterBag($str)) {
-            $str = $parameterBag['string'];
-            $bag_parameters = $parameterBag['params'];
-        }
-
-        if (!empty($parameters) || !empty($bag_parameters)) {
-            $echo_parameters="with {";
-
-            foreach ($parameters as $key => $value) {
-                $echo_parameters.= "'%".$key."%': '".$value."',";
-            }
-            foreach ($bag_parameters as $key => $value) {
-                $echo_parameters.= "'%".$key."%': ".$value.",";
-            }
-
-            $echo_parameters.="} ";
-        }
-
-        return '{% trans '.$echo_parameters.'from "'.$catalog.'" %}'.$str.'{% endtrans %}';
-    }
-
-    public function getEchoSet($var, $value, $value_as_string = true)
-    {
-        if ($value_as_string) {
-            return strtr('{% set %%var%% = "%%value%%" %}', array('%%var%%' => $var, '%%value%%' => $value));
-        } else {
-            return strtr('{% set %%var%% = %%value%% %}', array('%%var%%' => $var, '%%value%%' => $value));
-        }
-    }
-
-    public function getEchoPath($path, $params = null, $filters = null)
-    {
-        if (null === $params) {
-            return (null === $filters)
-                ? strtr('{{ path("%%path%%") }}', array('%%path%%' => $path))
-                : strtr(
-                    '{{ path("%%path%%")|%%filters%% }}',
-                    array(
-                        '%%path%%' => $path,
-                        '%%filters%%' => (is_array($filters) ? implode('|', $filters) : $filters)
-                    )
-                );
-        }
-
-        $params = preg_replace('/\{\{\s+?([\w\.]+)\s+?\}\}/i', '$1', $params);
-
-        return (null === $filters)
-            ? strtr('{{ path("%%path%%", %%params%%) }}', array('%%path%%' => $path, '%%params%%' => $params))
-            : strtr(
-                '{{ path("%%path%%", %%params%%)|%%filters%% }}',
-                array(
-                    '%%path%%' => $path,
-                    '%%params%%' => $params,
-                    '%%filters%%' => (is_array($filters) ? implode('|', $filters) : $filters)
-                )
-            );
-    }
-
-    public function getEchoIfGranted($credentials, $modelName = null)
-    {
-        if (null === $modelName) {
-            return $this->getEchoIf('is_expr_granted(\''.$credentials.'\')');
-        }
-
-        return $this->getEchoIf('is_expr_granted(\''.$credentials.'\', '.$modelName.')');
-    }
-
-    public function getEchoIf($condition)
-    {
-        if (is_bool($condition)) {
-            $condition = intval($condition);
-        }
-
-        return str_replace('%%condition%%', $condition, '{% if %%condition%% %}');
-    }
-
-    public function getEchoElseIf($condition)
-    {
-        if (is_bool($condition)) {
-            $condition = intval($condition);
-        }
-
-        return str_replace('%%condition%%', $condition, '{% elseif %%condition%% %}');
-    }
-
-    public function getEchoElse()
-    {
-        return '{% else %}';
-    }
-
-    public function getEchoEndIf()
-    {
-        return '{% endif %}';
-    }
-
-    public function getEchoTwig($str)
-    {
-        return sprintf('{{ %s }}', $str);
-    }
-
-    public function getEchoTwigFilter($str, $filters = null, $asString = false)
-    {
-        if (null === $filters) {
-            return $this->getEchoTwig($str);
-        }
-
-        return strtr(
-            '{{ %%str%%|%%filters%% }}',
-            array(
-                '%%str%%' => $asString ? '"'.$str.'"' : $str,
-                '%%filters%%' => (is_array($filters) ? implode('|', $filters) : $filters)
-            )
-        );
-    }
-
-    public function getEchoBlock($name)
-    {
-        $this->blockNames[] = $name;
-
-        return str_replace('%%name%%', $name, '{% block %%name%% %}');
-    }
-
-    public function getEchoExtends($name)
-    {
-        return str_replace('%%name%%', $name, '{% extends "%%name%%" %}');
-    }
-
-    public function getEchoEndBlock()
-    {
-        return str_replace('%%name%%', array_pop($this->blockNames), '{% endblock %%name%% %}');
-    }
-
-    public function getEchoFor($object, $in)
-    {
-        return strtr('{% for %%object%% in %%in%% %}', array('%%object%%' => $object, '%%in%%' => $in ));
-    }
-
-    public function getEchoEndFor()
-    {
-        return '{% endfor %}';
-    }
-
-    public function getEchoRaw()
-    {
-        return '{% raw %}';
-    }
-
-    public function getEchoEndRaw()
-    {
-        return '{% endraw %}';
-    }
-
-    public function getEchoSpaceless()
-    {
-        return '{% spaceless %}';
-    }
-
-    public function getEchoEndSpaceless()
-    {
-        return '{% endspaceless %}';
-    }
-
-    /**
-     * Converts an array to a twig array expression (string).
-     * Only in case a value contains '{{' and '}}' the value won't be
-     * wrapped in quotes.
-     *
-     * An array like:
-     * <code>
-     * $array = array('a' => 'b', 'c' => 'd');
-     * </code>
-     *
-     * Will be converted to:
-     * <code>
-     * "[ 'b', 'd']"
-     * </code>
-     *
-     * @return string The parameters to be used in a URL
-     */
-    public function getEchoTwigArr(array $arr)
-    {
-        $contents = array();
-        foreach ($arr as $key => $value) {
-            if (!strstr($value, '{{') || !strstr($value, '}}')) {
-                $value = "'$value'";
-            } else {
-                $value = trim(str_replace(array('{{', '}}'), '', $value));
-            }
-
-            $contents[] = "$value";
-        }
-
-        return '[ ' . implode(', ', $contents) . ' ]';
-    }
-
     /**
      * Converts an assoc array to a twig array expression (string) .
      * Only in case a value contains '{{' and '}}' the value won't be
@@ -610,20 +394,20 @@ class EchoExtension extends \Twig_Extension
      *
      * An array like:
      * <code>
-     * $array = array('a' => 'b', 'c' => 'd');
+     * $array = array('a' => 'b', 'c' => 'd', 'e' => '{{f}}');
      * </code>
      *
      * Will be converted to:
      * <code>
-     * "{ a: 'b', c: 'd' }"
+     * "{ a: 'b', c: 'd', e: f }"
      * </code>
      *
-     * @return string The parameters to be used in a URL
+     * @return string
      */
-    public function getEchoTwigAssoc(array $arr)
+    private function getTwigAssociativeArray(array $hashmap)
     {
         $contents = array();
-        foreach ($arr as $key => $value) {
+        foreach ($hashmap as $key => $value) {
             if (!strstr($value, '{{') || !strstr($value, '}}')) {
                 $value = "'$value'";
             } else {
@@ -636,33 +420,6 @@ class EchoExtension extends \Twig_Extension
         return '{ ' . implode(', ', $contents) . ' }';
     }
 
-    public function getEchoInclude($twig, array $params = array(), $paramsOnly = false)
-    {
-        return sprintf(
-            '{%% include "%s" with %s %s%%}',
-            $twig,
-            $this->getEchoTwigAssoc($params),
-            $paramsOnly ? 'only ' : ''
-        );
-    }
-
-    public function getEchoRender($controller, array $params = array())
-    {
-        $params = $this->getEchoTwigAssoc($params);
-
-        return '{{ render(controller("'.$controller.'", '.$params.')) }}';
-    }
-
-    public function getEchoUse($name)
-    {
-        return str_replace('%%name%%', $name, '{% use \'%%name%%\' %}');
-    }
-
-    public function getEchoPrintBlock($name)
-    {
-        return str_replace('%%name%%', $name, '{{ block(\'%%name%%\') }}');
-    }
-
     /**
      * Returns the name of the extension.
      *
@@ -670,6 +427,6 @@ class EchoExtension extends \Twig_Extension
      */
     public function getName()
     {
-        return 'echo';
+        return 'admingenerator_echo';
     }
 }
