symfony2admingenerator/GeneratorBundle

commit 8ebdf0d581101abdb8922b05c4684da98f08995b

Update Templates Dirs configuration FIX test

Generator/DoctrineGenerator.php
@@ -47,12 +47,7 @@ class DoctrineGenerator extends Generator
         );
         $generator->setFieldGuesser($this->getFieldGuesser());
         $generator->setMustOverwriteIfExists($this->needToOverwrite($generator));
-        $generator->setTemplateDirs(
-            array_merge(
-                $this->container->getParameter('admingenerator.doctrine_templates_dirs'),
-                array(__DIR__.'/../Resources/templates/Doctrine')
-            )
-        );
+        $generator->setTemplateDirs($this->templatesDirectories);
         $generator->setBaseController(
             'Admingenerator\GeneratorBundle\Controller\Doctrine\BaseController'
         );

Generator/DoctrineODMGenerator.php
@@ -45,12 +45,7 @@ class DoctrineODMGenerator extends Generator
         );
         $generator->setFieldGuesser($this->getFieldGuesser());
         $generator->setMustOverwriteIfExists($this->needToOverwrite($generator));
-        $generator->setTemplateDirs(
-            array_merge(
-                $this->container->getParameter('admingenerator.doctrineodm_templates_dirs'),
-                array(__DIR__.'/../Resources/templates/DoctrineODM')
-            )
-        );
+        $generator->setTemplateDirs($this->templatesDirectories);
         $generator->setBaseController(
             'Admingenerator\GeneratorBundle\Controller\DoctrineODM\BaseController'
         );

Generator/PropelGenerator.php
@@ -48,12 +48,7 @@ class PropelGenerator extends Generator
         );
         $generator->setFieldGuesser($this->getFieldGuesser());
         $generator->setMustOverwriteIfExists($this->needToOverwrite($generator));
-        $generator->setTemplateDirs(
-            array_merge(
-                $this->container->getParameter('admingenerator.propel_templates_dirs'),
-                array(__DIR__.'/../Resources/templates/Propel')
-            )
-        );
+        $generator->setTemplateDirs($this->templatesDirectories);
         $generator->setBaseController('Admingenerator\GeneratorBundle\Controller\Propel\BaseController');
         $generator->setColumnClass('Admingenerator\GeneratorBundle\Generator\PropelColumn');
         $generator->setBaseGeneratorName($this->getBaseGeneratorName());

DependencyInjection/AdmingeneratorGeneratorExtension.php
@@ -67,19 +67,22 @@ class AdmingeneratorGeneratorExtension extends Extension implements PrependExten
             throw new ModelManagerNotSelectedException();
         }
 
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__).DIRECTORY_SEPARATOR.'Resources'.DIRECTORY_SEPARATOR.'config'));
+        $config['templates_dirs'][] = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'templates';
 
-        // Fix template_dirs
-        $doctrine_template_dirs = $doctrineodm_template_dirs = $propel_template_dirs = array();
+        $doctrineOrmTemplatesDirs = array();
+        $doctrineOdmTemplatesDirs = array() ;
+        $propelTemplatesDirs = array();
         foreach ($config['templates_dirs'] as $dir) {
-            $doctrine_template_dirs[]    = $dir.'/Doctrine';
-            $doctrineodm_template_dirs[] = $dir.'/DoctrineODM';
-            $propel_template_dirs[]      = $dir.'/Propel';
+            $doctrineOrmTemplatesDirs[] = $dir.DIRECTORY_SEPARATOR.'Doctrine';
+            $doctrineOdmTemplatesDirs[] = $dir.DIRECTORY_SEPARATOR.'DoctrineODM';
+            $propelTemplatesDirs[]      = $dir.DIRECTORY_SEPARATOR.'Propel';
         }
+
 
         if ($config['use_doctrine_orm']) {
             $loader->load('doctrine_orm.xml');
-            $container->setParameter('admingenerator.doctrine_templates_dirs', $doctrine_template_dirs);
+            $this->addTemplatesInitialization($container->getDefinition('admingenerator.generator.doctrine'), $doctrineOrmTemplatesDirs);
 
             $formTypes = $config['form_types']['doctrine_orm'];
             $filterTypes = $config['filter_types']['doctrine_orm'];
@@ -89,7 +92,7 @@ class AdmingeneratorGeneratorExtension extends Extension implements PrependExten
 
         if ($config['use_doctrine_odm']) {
             $loader->load('doctrine_odm.xml');
-            $container->setParameter('admingenerator.doctrineodm_templates_dirs', $doctrineodm_template_dirs);
+            $this->addTemplatesInitialization($container->getDefinition('admingenerator.generator.doctrine_odm'), $doctrineOdmTemplatesDirs);
 
             $formTypes = $config['form_types']['doctrine_odm'];
             $filterTypes = $config['filter_types']['doctrine_odm'];
@@ -99,12 +102,29 @@ class AdmingeneratorGeneratorExtension extends Extension implements PrependExten
 
         if ($config['use_propel']) {
             $loader->load('propel.xml');
-            $container->setParameter('admingenerator.propel_templates_dirs', $propel_template_dirs);
+            $this->addTemplatesInitialization($container->getDefinition('admingenerator.generator.propel'), $propelTemplatesDirs);
 
             $formTypes = $config['form_types']['propel'];
             $filterTypes = $config['filter_types']['propel'];
             $container->setParameter('admingenerator.propel_form_types', $formTypes);
             $container->setParameter('admingenerator.propel_filter_types', $filterTypes);
+        }
+    }
+
+    /**
+     * Update $generatorDefinition to add calls to the addTemplatesDirectory
+     * for all $directories.
+     *
+     * @param Definition $generatorDefinition
+     * @param array $directories
+     */
+    private function addTemplatesInitialization(Definition $generatorDefinition, array $directories)
+    {
+        foreach ($directories as $directory) {
+            $generatorDefinition->addMethodCall(
+                'addTemplatesDirectory',
+                array($directory)
+            );
         }
     }
 

DependencyInjection/Configuration.php
@@ -72,7 +72,6 @@ class Configuration implements ConfigurationInterface
                     ->end()
                 ->end()
                 ->arrayNode('templates_dirs')
-                    ->useAttributeAsKey('key')
                     ->prototype('scalar')->end()
                 ->end()
                 ->arrayNode('form_types')

Generator/Generator.php
@@ -47,6 +47,15 @@ abstract class Generator extends ContainerAware implements GeneratorInterface
      */
     protected $cacheSuffix = 'default';
 
+    /**
+     * @var array
+     */
+    protected $templatesDirectories = array();
+
+    /**
+     * @param $root_dir
+     * @param $cache_dir
+     */
     public function __construct($root_dir, $cache_dir)
     {
         $this->root_dir = $root_dir;
@@ -64,21 +73,41 @@ abstract class Generator extends ContainerAware implements GeneratorInterface
         $this->cacheSuffix = $cacheSuffix;
     }
 
+    /**
+     * @param $directory
+     */
+    public function addTemplatesDirectory($directory)
+    {
+        $this->templatesDirectories[] = $directory;
+    }
+
+    /**
+     * @param $yaml_file
+     */
     public function setGeneratorYml($yaml_file)
     {
         $this->generator_yaml = $yaml_file;
     }
 
+    /**
+     * @return string
+     */
     public function getGeneratorYml()
     {
         return $this->generator_yaml;
     }
 
+    /**
+     * @param $base_generator_name
+     */
     public function setBaseGeneratorName($base_generator_name)
     {
         $this->base_generator_name = $base_generator_name;
     }
 
+    /**
+     * @return string
+     */
     protected function getBaseGeneratorName()
     {
         return $this->base_generator_name;

Tests/Twig/Extension/EchoExtensionTest.php
@@ -534,7 +534,7 @@ class EchoExtensionTest extends TestCase
             'several_values' => array("[ 'abcde', '12345' ]", 'Transform correctly array with values'),
             'incomplete_variables' => array("[ '{{ Item.id ' ]", 'Variables with unclosed {{ }} are quoted'),
             'complete_valiables' => array("[ Item.id ]", 'Variables are passed w/o surrounding {{ }}'),
-            'mixed_values' => array("[ Item.id, 'Item.id }}', 'abcde' }", 'Several values in the same expression'),
+            'mixed_values' => array("[ Item.id, 'Item.id }}', 'abcde' ]", 'Several values in the same expression'),
         );
 
         $this->runTwigTests($tpls, $returns);

commit f6af26f9cdea442fc984880e82bc2db8e03970c2

FIX error introduced by commit 8ebdf0d581101abdb8922b05c4684da98f08995b

Generator/DoctrineGenerator.php
@@ -139,12 +139,7 @@ class DoctrineGenerator extends Generator
         );
         $embedGenerator->setFieldGuesser($this->getFieldGuesser());
         $embedGenerator->setMustOverwriteIfExists($this->needToOverwrite($embedGenerator));
-        $embedGenerator->setTemplateDirs(
-            array_merge(
-                $this->container->getParameter('admingenerator.doctrine_templates_dirs'),
-                array(__DIR__.'/../Resources/templates/Doctrine')
-            )
-        );
+        $embedGenerator->setTemplateDirs($this->templatesDirectories);
 
         $embedGenerator->addBuilder(new EditBuilderType());
         $embedGenerator->addBuilder(new NewBuilderType());

Generator/DoctrineODMGenerator.php
@@ -132,12 +132,7 @@ class DoctrineODMGenerator extends Generator
         );
         $embedGenerator->setFieldGuesser($this->getFieldGuesser());
         $embedGenerator->setMustOverwriteIfExists($this->needToOverwrite($embedGenerator));
-        $embedGenerator->setTemplateDirs(
-            array_merge(
-                $this->container->getParameter('admingenerator.doctrineodm_templates_dirs'),
-                array(__DIR__.'/../Resources/templates/DoctrineODM')
-            )
-        );
+        $embedGenerator->setTemplateDirs($this->templatesDirectories);
 
         $embedGenerator->addBuilder(new EditBuilderType());
         $embedGenerator->addBuilder(new NewBuilderType());

Generator/PropelGenerator.php
@@ -139,13 +139,8 @@ class PropelGenerator extends Generator
         );
         $embedGenerator->setFieldGuesser($this->getFieldGuesser());
         $embedGenerator->setMustOverwriteIfExists($this->needToOverwrite($embedGenerator));
-        $embedGenerator->setTemplateDirs(
-            array_merge(
-                $this->container->getParameter('admingenerator.propel_templates_dirs'),
-                array(__DIR__.'/../Resources/templates/Propel')
-            )
-        );
-         $embedGenerator->setColumnClass('Admingenerator\GeneratorBundle\Generator\PropelColumn');
+        $embedGenerator->setTemplateDirs($this->templatesDirectories);
+        $embedGenerator->setColumnClass('Admingenerator\GeneratorBundle\Generator\PropelColumn');
 
         $embedGenerator->addBuilder(new EditBuilderType());
         $embedGenerator->addBuilder(new NewBuilderType());