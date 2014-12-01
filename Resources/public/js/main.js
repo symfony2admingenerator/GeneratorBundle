a17b818ea5a056ae130f3644dea85aff75025e95
Stéphane Escandell <stephane.escandell@gmail.com>
2014-12-01 13:53:04 +0100

    Split JS files, use Bower

---
+++ Resources/public/js/main.js
@@ -0,0 +1,9 @@
+// Custom scripts belonging to Admingenerator
+;(function(window, $, undefined){
+
+    // On load handler
+    $(function(){
+        // Force first clicks on tabs
+        $('.nav-tabs *[data-toggle="tab"]:first').click();
+    });
+})(window, jQuery);
