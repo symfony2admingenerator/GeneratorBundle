bb5e43f5931b9b04dd0f962f2a4fc639f554812e
Stéphane Escandell <stephane.escandell@gmail.com>
2014-12-07 23:27:48 +0100

    Batch actions + iCheck on list + making object / generic / batch actions JS options customizable

--- Resources/public/js/main.js
+++ Resources/public/js/main.js
@@ -3,11 +3,17 @@
     var S2A = window.S2A || {};
     window.S2A = S2A;
 
-    S2A.actionsManager = function(container, selector){
-        $(container).on('click', selector, this.clickHandler.bind(this));
+    S2A.singleActionsManager = function(options){
+        this.options = $.extend({}, {
+                containerSelector: 'document',
+                buttonSelector: 'a.object-action'
+            },
+            options
+        );
+        $(this.options.containerSelector).on('click', this.options.buttonSelector, this.clickHandler.bind(this));
     };
 
-    S2A.actionsManager.prototype = {
+    S2A.singleActionsManager.prototype = {
         clickHandler: function(evt){
             var $elt = $(evt.currentTarget);
 
@@ -24,7 +30,7 @@
 
         isConfirmed: function($elt){
             // TODO: move confirm() to custom popin
-            return !$elt.data('confirm') || confirm($elt.data('confirm'))
+            return !$elt.data('confirm') || confirm($elt.data('confirm'));
         },
 
         isProtected: function($elt){
@@ -50,9 +56,88 @@
         }
     };
 
+    S2A.batchActionsManager = function(options){
+        this.options = $.extend( {}, {
+                containerSelector: 'document',
+                actionSelector: 'select[name=action]',
+                toggleSelector: 'input[name=batch_select_all]',
+                elementSelector: 'input[name="selected[]"]',
+                noActionValue: 'none',
+                noElementSelectedMessage: 'You have to select at least one element.'
+            },
+            options
+        );
+
+        this.allElementsToggleButton = $(this.options.containerSelector).find(this.options.toggleSelector);
+        this.actionInputSelector = $(this.options.containerSelector).find(this.options.actionSelector);
+        $(this.options.containerSelector).find('*[type=submit]').hide();
+
+        this.actionInputSelector.on('change', this.selectedActionChangedHandler.bind(this));
+        this.allElementsToggleButton.on('ifChecked ifUnchecked', this.allElementsChangedHandler.bind(this));
+    };
+
+    S2A.batchActionsManager.prototype = {
+        selectedActionChangedHandler: function(evt){
+            var $elt = $(evt.currentTarget);
+            if (!this.isValidActionSelected($elt.val())) {
+                return;
+            }
+
+            if (!this.hasElementsSelected()) {
+                // TODO: move this to a popin or dynamic message displayer
+                alert(this.options.noElementSelectedMessage);
+                $elt.val(this.options.noActionValue);
+                return;
+            }
+
+            if (!this.isConfirmed($elt)) {
+                $elt.val(this.options.noActionValue);
+                return;
+            }
+
+            // TODO: pre-submit trigger
+            // Send the form
+            $elt[0].form.submit();
+        },
+
+        allElementsChangedHandler: function(evt){
+            $(this.options.elementSelector)
+                .prop('checked', $(evt.currentTarget).is(':checked'))
+                .iCheck('update');
+        },
+
+        isValidActionSelected: function(actionValue){
+            return actionValue != this.options.noActionValue;
+        },
+
+        hasElementsSelected: function(){
+            return 0 !== $(this.options.containerSelector + ' ' + this.options.elementSelector).filter(':checked').length;
+        },
+
+        isConfirmed: function($elt){
+            var $selectedOption = $(':selected', $elt);
+
+            if (0 == $selectedOption.length) {
+                return false;
+            }
+
+            return !$selectedOption.data('confirm') || confirm($selectedOption.data('confirm'));
+        }
+    };
+
     // Force first tab to be displayed
     $('.nav-tabs *[data-toggle="tab"]:first').click();
 
-    // Object and Generic actions
-    new S2A.actionsManager('section.content', '.object-action, .generic-action');
+    // Object actions
+    if (S2A.hasOwnProperty('singleActionsAdminOptions')) {
+        new S2A.singleActionsManager(S2A.singleActionsAdminOptions);
+    }
+    // Generic actions
+    if (S2A.hasOwnProperty('genericActionsAdminOptions')) {
+        new S2A.singleActionsManager(S2A.genericActionsAdminOptions);
+    }
+    // Batch actions
+    if (S2A.hasOwnProperty('batchActionsAdminOptions')) {
+        new S2A.batchActionsManager(S2A.batchActionsAdminOptions);
+    }
 })(window, jQuery);
