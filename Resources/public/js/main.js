1039e5b25189dd602a524ddad6b1380837a2dfac
Stéphane Escandell <stephane.escandell@gmail.com>
2014-12-07 17:51:23 +0100

    Manage object actions as the legacy bundle

--- Resources/public/js/main.js
+++ Resources/public/js/main.js
@@ -1,9 +1,33 @@
 // Custom scripts belonging to Admingenerator
 ;(function(window, $, undefined){
+    // Force first tab to be displayed
+    $('.nav-tabs *[data-toggle="tab"]:first').click();
 
-    // On load handler
-    $(function(){
-        // Force first clicks on tabs
-        $('.nav-tabs *[data-toggle="tab"]:first').click();
+    // Object actions handler
+    $('section.content').on('click', 'a.object-action', function(evt){
+        var $elt = $(this);
+
+        // TODO: move this to custom popin
+        if ($elt.data('confirm') && !confirm($elt.data('confirm'))) {
+            evt.preventDefault();
+            return;
+        }
+
+        if ($elt.data('csrf-token')) {
+            evt.preventDefault();
+            // Transform in POST request
+            var $form = $('<form />').attr({
+                method: 'POST',
+                action: $elt.attr('href'),
+                style:  'visibility: hidden'
+            }).appendTo($('body'));
+            // Add CSRF protection token
+            $('<input />').attr({
+                type:   'hidden',
+                name:   '_csrf_token',
+                value:  $elt.data('csrf-token')
+            }).appendTo($form);
+            $form.submit();
+        }
     });
 })(window, jQuery);
