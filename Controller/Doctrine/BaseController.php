aae464dc1c31650bd0e609ac529ce78362aa1fbf
Tobias Feijten <tobias@kick-in.nl>
2021-12-28 19:53:17 +0100

    No longer use deprecated getDoctrine()

--- Controller/Doctrine/BaseController.php
+++ Controller/Doctrine/BaseController.php
@@ -2,6 +2,7 @@
 
 namespace Admingenerator\GeneratorBundle\Controller\Doctrine;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,6 +19,16 @@ abstract class BaseController extends AbstractController
      * @var Request
      */
     protected $request;
+
+    /**
+     * @var ManagerRegistry
+     */
+    protected $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+      $this->doctrine = $doctrine;
+    }
 
     /**
      * Ensure the translator and logger services are available for usage
