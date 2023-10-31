fd9529903fbdd59cd56204cfb919ec4ea96c3313
Bob van de Vijver <bobvandevijver@hotmail.com>
2023-10-31 21:03:34 +0100

    Resolve container get deprecations

--- Controller/Propel/BaseController.php
+++ Controller/Propel/BaseController.php
@@ -2,33 +2,13 @@
 
 namespace Admingenerator\GeneratorBundle\Controller\Propel;
 
-use Psr\Log\LoggerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Admingenerator\GeneratorBundle\Controller\AdminBaseController;
 
 /**
  * A base controller for Propel
  *
  * @author cedric Lombardot
- *
  */
-abstract class BaseController extends AbstractController
+abstract class BaseController extends AdminBaseController
 {
-    protected ?Request $request = null;
-
-    /**
-     * Ensure the translator and logger services are available for usage
-     */
-    public static function getSubscribedServices(): array
-    {
-        return array_merge(
-            parent::getSubscribedServices(),
-            [
-                'translator' => interface_exists('Symfony\Contracts\Translation\TranslatorInterface')
-                    ? \Symfony\Contracts\Translation\TranslatorInterface::class
-                    : \Symfony\Component\Translation\TranslatorInterface::class,
-                'logger' => LoggerInterface::class,
-            ]
-        );
-    }
 }
