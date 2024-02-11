8c5e73f02ab21b069388a9a645541aebda1a4b5f
Bob van de Vijver <bobvandevijver@hotmail.com>
2024-02-11 19:57:14 +0100

    Fix twig generator template file

--- Resources/skeleton/template/twig_generator.tpl.php
+++ Resources/skeleton/template/twig_generator.tpl.php
@@ -1,1 +1,1 @@
-{% extends_admingenerated "<?= $bundle ?>:<?= $prefix ?><?= $action ?>:<?= $view ?>.html.twig" %}}
+{% extends_admingenerated "<?= $bundle ?>:<?= $prefix ?><?= $action ?>:<?= $view ?>.html.twig" %}
